d27b247ea123238a949b4ce0fa08e1141b2ee17a
RyanRafaelA <ryrafael461@gmail.com>
2025-02-06 20:13:26 -0300

    Menu interativo construido

--- exercicioFuncaoCarro/src/Main.java
+++ exercicioFuncaoCarro/src/Main.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        int opc = 0;
+
+        while(opc != 8){
+            menuInterativo();
+            opc = sc.nextInt();
+
+            switch(opc){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                default:
+                    System.out.println("Não existe essa opção.");
+            }
+        }
+    }
+
+    static void menuInterativo(){
+        System.out.println("===========================================");
+        System.out.println("[1] Ligar Carro");
+        System.out.println("[2] Desligar Carro");
+        System.out.println("[3] Acelarar");
+        System.out.println("[4] Diminuir velocidade");
+        System.out.println("[5] Virar para esquerda/direita");
+        System.out.println("[6] Verificar Velocidade");
+        System.out.println("[7] Trocar Marcha");
+        System.out.println("[8] sair");
+        System.out.print("Digite a opção: ");
     }
 }
