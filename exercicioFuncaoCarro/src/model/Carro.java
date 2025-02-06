2b8b9419557c3244fd21e582dfaacffad862433f
RyanRafaelA <ryrafael461@gmail.com>
2025-02-06 20:19:46 -0300

    Classe Carro inicializado

---
+++ exercicioFuncaoCarro/src/model/Carro.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Carro {
+    private boolean carroLigado;
+    private int velocidade;
+    private int marcha;
+
+    public Carro(){
+        carroLigado = false;
+        marcha = 0;
+        velocidade = 0;
+    }
+}
